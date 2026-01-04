38d2927b95136e57a3854cb2c03394bb11884c9b
Nelson R Monserrate <nrmonserrate@gmail.com>
2026-01-04 21:53:12 +0100

    refactor(shell): consolidate bash and zsh configs into shared includes
    Create shared/shell/ directory with common aliases and functions that work
    in both shells. Symlink from bash and zsh include directories to reduce
    duplication and ensure consistency.

--- shared/shell/git.sh
+++ shared/shell/git.sh
@@ -1,26 +1,28 @@
-# shellcheck disable=all
+# Git aliases - inspired by prezto git module
+# https://github.com/sorin-ionescu/prezto/blob/master/modules/git/alias.zsh
 
-# Heavily inspired from https://github.com/sorin-ionescu/prezto/blob/master/modules/git/alias.zsh
-# Git aliases
+# ------------------------------------------------------------------------------
+# Log Formats
+# ------------------------------------------------------------------------------
 
-# Log Format
-zstyle -s ":prezto:module:git:log:medium" format "_git_log_medium_format" ||
-    _git_log_medium_format="%C(bold)Commit:%C(reset) %C(green)%H%C(bold magenta)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%w(80,1,2)%+B"
-zstyle -s ":prezto:module:git:log:oneline" format "_git_log_oneline_format" ||
-    _git_log_oneline_format="%C(green)%h%C(reset) %C(blue)%ad%C(reset) %C(cyan)%an%C(reset) %C(bold magenta)%d%C(reset) %s"
-zstyle -s ":prezto:module:git:log:brief" format "_git_log_brief_format" ||
-    _git_log_brief_format="%C(green)%h%C(reset) %C(blue)(%ar by %C(cyan)%an%C(blue))%C(bold magenta)%d%C(reset) %n%s%n"
+_git_log_medium_format="%C(bold)Commit:%C(reset) %C(green)%H%C(bold magenta)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%w(80,1,2)%+B"
+_git_log_oneline_format="%C(green)%h%C(reset) %C(blue)%ad%C(reset) %C(cyan)%an%C(reset) %C(bold magenta)%d%C(reset) %s"
+_git_log_brief_format="%C(green)%h%C(reset) %C(blue)(%ar by %C(cyan)%an%C(blue))%C(bold magenta)%d%C(reset) %n%s%n"
+_git_status_ignore_submodules="none"
 
-# Status
-zstyle -s ":prezto:module:git:status:ignore" submodules "_git_status_ignore_submodules" ||
-    _git_status_ignore_submodules="none"
+# ------------------------------------------------------------------------------
+# Git / TUI
+# ------------------------------------------------------------------------------
 
 alias g="git"
 alias gui="gitui"
 alias glz="lazygit"
 alias lzg="lazygit"
 
+# ------------------------------------------------------------------------------
 # Branch (b)
+# ------------------------------------------------------------------------------
+
 alias gb="git branch"
 alias gbv="git branch --verbose --verbose"
 alias gba="git branch --all"
@@ -35,7 +37,10 @@ alias gbM="git branch --move --force"
 alias gbs="git show-branch"
 alias gbsa="git show-branch --all"
 
+# ------------------------------------------------------------------------------
 # Commit (c)
+# ------------------------------------------------------------------------------
+
 alias gc="git commit --verbose"
 alias gcS="git commit --verbose --gpg-sign"
 alias gca="git commit --verbose --all"
@@ -52,36 +57,48 @@ alias gcFS="git commit --verbose --amend --gpg-sign"
 alias gcp="git cherry-pick --ff"
 alias gcP="git cherry-pick --no-commit"
 alias gcr="git revert"
-alias gcR="git reset \"HEAD^\""
+alias gcR='git reset "HEAD^"'
 alias gcs="git show"
 alias gcsS="git show --pretty=short --show-signature"
 alias gcl="git-commit-lost"
 alias gcy="git cherry --verbose --abbrev"
 alias gcY="git cherry --verbose"
 
+# ------------------------------------------------------------------------------
 # Rev-parse (rp)
+# ------------------------------------------------------------------------------
+
 alias grpl="git rev-parse HEAD | tr -d '\n'"
 alias grps="git rev-parse HEAD | cut -c1-7 | tr -d '\n'"
 
+# ------------------------------------------------------------------------------
 # Conflict (C)
-alias gCl="git --no-pager diff --name-only --diff-filter=U"
-alias gCa="git add \$(gCl)"
-alias gCe="git mergetool \$(gCl)"
-alias gCo="git checkout --ours --"
-alias gCO="gCo \$(gCl)"
-alias gCt="git checkout --theirs --"
-alias gCT="gCt \$(gCl)"
+# ------------------------------------------------------------------------------
 
+alias gCl="git --no-pager diff --name-only --diff-filter=U"
+alias gCa='git add $(gCl)'
+alias gCe='git mergetool $(gCl)'
+alias gCo="git checkout --ours --"
+alias gCO='gCo $(gCl)'
+alias gCt="git checkout --theirs --"
+alias gCT='gCt $(gCl)'
+
+# ------------------------------------------------------------------------------
 # Data (d)
+# ------------------------------------------------------------------------------
+
 alias gd="git ls-files"
 alias gdc="git ls-files --cached"
 alias gdx="git ls-files --deleted"
 alias gdm="git ls-files --modified"
 alias gdu="git ls-files --other --exclude-standard"
 alias gdk="git ls-files --killed"
-alias gdi="git status --porcelain --short --ignored | sed -n \"s/^!! //p\""
+alias gdi='git status --porcelain --short --ignored | sed -n "s/^!! //p"'
 
+# ------------------------------------------------------------------------------
 # Fetch (f)
+# ------------------------------------------------------------------------------
+
 alias gf="git fetch"
 alias gfa="git fetch --all --prune --tags"
 alias gfc="git clone"
@@ -91,7 +108,10 @@ alias gfma="git pull --autostash"
 alias gfr="git pull --rebase"
 alias gfra="git pull --rebase --autostash"
 
+# ------------------------------------------------------------------------------
 # Grep (g)
+# ------------------------------------------------------------------------------
+
 alias gg="git grep"
 alias ggi="git grep --ignore-case"
 alias ggl="git grep --files-with-matches"
@@ -99,7 +119,10 @@ alias ggL="git grep --files-without-matches"
 alias ggv="git grep --invert-match"
 alias ggw="git grep --word-regexp"
 
+# ------------------------------------------------------------------------------
 # Index (i)
+# ------------------------------------------------------------------------------
+
 alias gia="git add"
 alias giA="git add --patch"
 alias giu="git add --update"
@@ -112,7 +135,10 @@ alias giR="git reset --patch"
 alias gix="git rm -r --cached"
 alias giX="git rm -r --force --cached"
 
+# ------------------------------------------------------------------------------
 # Log (l)
+# ------------------------------------------------------------------------------
+
 alias gl='git log --topo-order --pretty=format:"$_git_log_medium_format"'
 alias gls='git log --topo-order --stat --pretty=format:"$_git_log_medium_format"'
 alias gld='git log --topo-order --stat --patch --full-diff --pretty=format:"$_git_log_medium_format"'
@@ -122,31 +148,43 @@ alias glb='git log --topo-order --pretty=format:"$_git_log_brief_format"'
 alias glc='git shortlog --summary --numbered'
 alias glS='git log --show-signature'
 
+# ------------------------------------------------------------------------------
 # Merge (m)
+# ------------------------------------------------------------------------------
+
 alias gm="git merge"
 alias gmC="git merge --no-commit"
 alias gmF="git merge --no-ff"
 alias gma="git merge --abort"
 alias gmt="git mergetool"
 
+# ------------------------------------------------------------------------------
 # Push (p)
+# ------------------------------------------------------------------------------
+
 alias gp="git push"
 alias gpf="git push --force-with-lease"
 alias gpF="git push --force"
 alias gpa="git push --all"
 alias gpA="git push --all && git push --tags"
 alias gpt="git push --tags"
-alias gpc='git push --set-upstream origin "$(git-branch-current 2> /dev/null)"'
-alias gpp='git pull origin "$(git-branch-current 2> /dev/null)" && git push origin "$(git-branch-current 2> /dev/null)"'
+alias gpc='git push --set-upstream origin "$(git-branch-current 2>/dev/null)"'
+alias gpp='git pull origin "$(git-branch-current 2>/dev/null)" && git push origin "$(git-branch-current 2>/dev/null)"'
 
+# ------------------------------------------------------------------------------
 # Rebase (r)
+# ------------------------------------------------------------------------------
+
 alias gr="git rebase"
 alias gra="git rebase --abort"
 alias grc="git rebase --continue"
 alias gri="git rebase --interactive"
 alias grs="git rebase --skip"
 
+# ------------------------------------------------------------------------------
 # Remote (R)
+# ------------------------------------------------------------------------------
+
 alias gR="git remote"
 alias gRl="git remote --verbose"
 alias gRa="git remote add"
@@ -158,36 +196,33 @@ alias gRp="git remote prune"
 alias gRs="git remote show"
 alias gRb="git-hub-browse"
 
+# ------------------------------------------------------------------------------
 # Stash (s)
+# ------------------------------------------------------------------------------
+
 alias gs="git stash"
 alias gsa="git stash apply"
 alias gsx="git stash drop"
 alias gsl="git stash list"
 alias gsd="git stash show --patch --stat"
 alias gsp="git stash pop"
-alias gss="git stash save --include-untracked"
-alias gsS="git stash save --patch --no-keep-index"
-alias gsw="git stash save --include-untracked --keep-index"
+alias gss="git stash push --include-untracked"
+alias gsS="git stash push --patch --no-keep-index"
+alias gsw="git stash push --include-untracked --keep-index"
 
-# Submodule (S)
-alias gS="git submodule"
-alias gSa="git submodule add"
-alias gSf="git submodule foreach"
-alias gSi="git submodule init"
-alias gSI="git submodule update --init --recursive"
-alias gSl="git submodule status"
-alias gSm="git-submodule-move"
-alias gSs="git submodule sync"
-alias gSu="git submodule update --remote --recursive"
-alias gSx="git-submodule-remove"
-
+# ------------------------------------------------------------------------------
 # Tag (t)
+# ------------------------------------------------------------------------------
+
 alias gt="git tag"
 alias gtl="git tag --list"
 alias gts="git tag --sign"
 alias gtv="git verify-tag"
 
+# ------------------------------------------------------------------------------
 # Working Copy (w)
+# ------------------------------------------------------------------------------
+
 alias gws='git status --ignore-submodules=$_git_status_ignore_submodules --short'
 alias gwS='git status --ignore-submodules=$_git_status_ignore_submodules'
 alias gwd="git diff --no-ext-diff"
